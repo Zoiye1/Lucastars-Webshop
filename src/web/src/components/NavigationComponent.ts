d17016d8fe3b58295d86e3ce1d32e215420c8754
Miguel Jesus Madeira <miguel.jesus.madeira@hva.nl>
2025-04-15 20:49:01 +0200

    feat(web): add styling, SVG icons and links in NavigationComponent

--- src/web/src/components/NavigationComponent.ts
+++ src/web/src/components/NavigationComponent.ts
@@ -12,15 +12,60 @@ export class NavigationComponent extends HTMLElement {
             return;
         }
 
+        const styles: HTMLElement = html`
+            <style>
+                nav {
+                    display: flex;
+                    gap: 16px;
+                }
+
+                nav a {
+                    display: flex;
+                    align-items: center;
+                    gap: 6px;
+                    padding: 10px 0;
+                    text-decoration: none;
+                    color: black;
+                }
+
+                nav a:hover {
+                    text-decoration: underline;
+                }
+
+                nav img {
+                    height: 24px;
+                    width: 24px;
+                    user-select: none;
+                }
+            </style>
+        `;
+
         const element: HTMLElement = html`
             <nav>
-                <a href="/index.html">Home</a>
-                <a href="/example.html">Example</a>
+                <a href="/index.html">
+                    <img src="/assets/images/icons/gamepad.svg" alt="Gamepad icon" />
+                    <span>Mijn spellen</span>
+                </a>
+
+                <a href="/index.html">
+                    <img src="/assets/images/icons/account.svg" alt="Account icon" />
+                    <span>Account</span>
+                </a>
+
+                <a href="/index.html">
+                    <img src="/assets/images/icons/cart.svg" alt="Cart icon" />
+                    <span>Winkelmand (0)</span>
+                </a>
+
+                <a href="/example.html">
+                    <img src="/assets/images/icons/question.svg" alt="Question icon" />
+                    <span>Example</span>
+                </a>
             </nav>
         `;
 
         this.shadowRoot.firstChild?.remove();
-        this.shadowRoot.append(element);
+        this.shadowRoot.append(styles, element);
     }
 }
 
