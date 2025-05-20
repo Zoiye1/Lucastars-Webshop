57a902432cb1ff863147894cc6c1d4c8201854ec
Omar Alhasan <omar.alhasan@hva.nl>
2025-05-20 13:24:00 +0200

    fix(cart): update createCart method signature to enforce type safety

--- src/api/src/interfaces/ICartService.ts
+++ src/api/src/interfaces/ICartService.ts
@@ -1,9 +1,7 @@
-import { CartItem } from "@shared/types";
+import { Cart, CartItem } from "@shared/types";
 
 export abstract class ICartService {
-    createCart(userId: number | undefined, gameId: any, quantity: any) {
-        throw new Error("Method not implemented.");
-    }
+    public abstract createCart(userId: number | undefined, gameId: number, quantity: number): Promise<Cart[] | undefined>;
     public abstract getCart(userId: number): Promise<CartItem[]>;
     public abstract updateCart(userId: number, items: CartItem[]): void;
     public abstract deleteCartItem(userId: number, gameId: number): Promise<void>;
